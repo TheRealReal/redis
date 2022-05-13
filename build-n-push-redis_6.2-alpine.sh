2bd34c3ce945e4954adc57eddb63151d43a82ce1
jroberts235 <jroberts@yeehaaa.com>
2022-05-13 15:00:04 -0700

    added script to build the Docker container and push to ECR in TRRs Production AWS account.

---
+++ build-n-push-redis_6.2-alpine.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+AWS=$(which aws)
+DOCKER=$(which docker)
+
+cd 6.2/alpine/ || exit 0
+PASSWD=$($AWS ecr get-login-password --region us-east-1) && \
+$DOCKER login -u AWS -p "$PASSWD" 701240562969.dkr.ecr.us-east-1.amazonaws.com && \
+$DOCKER build -f Dockerfile -t redis-6.2.6-alpine . && \
+$DOCKER tag redis-6.2.6-alpine:latest 701240562969.dkr.ecr.us-east-1.amazonaws.com/redis:latest && \
+$DOCKER push 701240562969.dkr.ecr.us-east-1.amazonaws.com/redis:latest
