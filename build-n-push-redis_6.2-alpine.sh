16a9ea9a6c00fc7969bdf02c833dfd7515c1af16
jroberts235 <jroberts@yeehaaa.com>
2022-05-13 15:07:23 -0700

    changed the image tag from "latest" to "6.2.6-alpine"

--- build-n-push-redis_6.2-alpine.sh
+++ build-n-push-redis_6.2-alpine.sh
@@ -6,5 +6,5 @@ cd 6.2/alpine/ || exit 0
 PASSWD=$($AWS ecr get-login-password --region us-east-1) && \
 $DOCKER login -u AWS -p "$PASSWD" 701240562969.dkr.ecr.us-east-1.amazonaws.com && \
 $DOCKER build -f Dockerfile -t redis-6.2.6-alpine . && \
-$DOCKER tag redis-6.2.6-alpine:latest 701240562969.dkr.ecr.us-east-1.amazonaws.com/redis:latest && \
-$DOCKER push 701240562969.dkr.ecr.us-east-1.amazonaws.com/redis:latest
+$DOCKER tag redis-6.2.6-alpine:latest 701240562969.dkr.ecr.us-east-1.amazonaws.com/redis:6.2.6-alpine && \
+$DOCKER push 701240562969.dkr.ecr.us-east-1.amazonaws.com/redis:6.2.6-alpine
